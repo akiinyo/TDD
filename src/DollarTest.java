add44ea66bca284202ccbb14d6b1044014f2affc
Tagaki Aki <a-tagaki@esm.co.jp>
2011-07-28 09:51:14 +0900

    chapter3.

--- src/DollarTest.java
+++ src/DollarTest.java
@@ -10,4 +10,9 @@ public class DollarTest extends TestCase {
 		product = five.times(3);
 		assertEquals(15,product.amount);
 	}
+	
+	public void testEquality(){
+		assertTrue(new Dollar(5).equals(new Dollar(5)));
+		assertFalse(new Dollar(5).equals(new Dollar(6)));
+	}
 }
