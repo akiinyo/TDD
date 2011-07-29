bbe5f805b8823aac516fb5d50eeb3de733dc0167
Tagaki Aki <a-tagaki@esm.co.jp>
2011-07-29 12:29:41 +0900

    chapter 4.

--- src/DollarTest.java
+++ src/DollarTest.java
@@ -5,14 +5,13 @@ public class DollarTest extends TestCase {
 	 
 	public void testMultiplication() {
 		Dollar five = new Dollar(5);
-		Dollar product = five.times(2);
-		assertEquals(10,product.amount);
-		product = five.times(3);
-		assertEquals(15,product.amount);
+		assertEquals(new Dollar(10),five.times(2));
+		assertEquals(new Dollar(15),five.times(3));
 	}
 	
 	public void testEquality(){
 		assertTrue(new Dollar(5).equals(new Dollar(5)));
 		assertFalse(new Dollar(5).equals(new Dollar(6)));
 	}
+	
 }
