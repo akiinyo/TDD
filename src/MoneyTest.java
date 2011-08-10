abf2b1f75740786651b53b321b7d4d55d528b388
Tagaki Aki <a-tagaki@esm.co.jp>
2011-08-10 11:53:29 +0900

    chapter 11.

---
+++ src/MoneyTest.java
@@ -0,0 +1,22 @@
+import junit.framework.TestCase;
+
+
+public class MoneyTest extends TestCase {
+	 
+	public void testDollarMultiplication() {
+		Money five = Money.dollar(5);
+		assertEquals(Money.dollar(10),five.times(2));
+		assertEquals(Money.dollar(15),five.times(3));
+	}
+	
+	public void testEquality(){
+		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+		assertFalse(Money.franc(5).equals(Money.dollar(5)));
+	}
+	
+	public void testCurrency(){
+		assertEquals("USD",Money.dollar(1).currency());
+		assertEquals("CHF",Money.franc(1).currency());
+	}
+}
