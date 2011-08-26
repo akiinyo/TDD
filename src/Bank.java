a0791bdb5c32f34d80c5a14b7cdcc1e2a595ee72
Tagaki Aki <a-tagaki@esm.co.jp>
2011-08-26 09:37:28 +0900

    chapter 14.

--- src/Bank.java
+++ src/Bank.java
@@ -1,6 +1,21 @@
+import java.util.Hashtable;
+
 
 public class Bank {
-	Money reduce(Expression source,String to){
-		return source.reduce(to);
+	private Hashtable<Pair, Integer> rates= new Hashtable<Pair, Integer>();
+	
+	public Money reduce(Expression source,String to){
+		return source.reduce(this,to);
 	}
+
+	public int rate(String from, String to){
+		if(from.equals(to)) return 1;
+		Integer rate= (Integer) rates.get(new Pair(from, to));
+		return rate.intValue();
+		
+	}
+	
+	public void addRate(String from, String to, int rate) {
+		rates.put(new Pair(from, to), rate);	
+	}	
 }
--- src/Money.java
+++ src/Money.java
@@ -36,8 +36,9 @@ class Money implements Expression{
 		return new Sum (this, addend);	
 	}
 	
-	public Money reduce(String to){
-		return this;
+	public Money reduce(Bank bank, String to){
+		int rate = bank.rate(currency, to);
+		return new Money(amount / rate, to);
 	}
 
 }
